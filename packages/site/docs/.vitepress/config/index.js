9a937935e5690357cdf753b2e2e706e306a250c5
yuqi.pyq <pyqiverson@gmail.com>
2024-05-14 10:21:43 +0800

    chore: add base url

--- packages/site/docs/.vitepress/config/index.js
+++ packages/site/docs/.vitepress/config/index.js
@@ -7,6 +7,7 @@ import { en } from './en';
 import { zh } from './zh';
 
 export default defineConfig({
+  base: '/infinite-canvas-tutorial/',
   markdown: {
     config: (md) => {
       md.use(implicitFigures, {
